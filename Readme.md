a75b16f2349aa89e4242beb7d6704385dacc9289
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-08 15:47:20 +0700

    feat: create dark mode theme toggle button

--- Readme.md
+++ Readme.md
@@ -18,3 +18,8 @@ Developer Universe is a social media application similar to Discord with chat, s
 - #1e2736 [Navy/Base-Color]
 - #fc4404 [Orange/Secondary-Color]
 - #ffffff [White/Tertiary-Color]
+
+## Feature 
+
+- Authentication Using Clerk [Login-Logout] 
+- Dark Mode [Shadcn-UI]
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
       <UserButton 
         afterSignOutUrl="/"
       />
+      <ModeToggle>
+        
+      </ModeToggle>
     </div>
   )
 }
