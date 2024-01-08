f0e971ad7d614d7c1ff333502bd1b331a2ce5e12
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-08 13:54:27 +0700

    feat: environtment setup & testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+
 export default function Home() {
   return (
-    <p className='text-3xl font-bold text-indigo-500'>
-      Hello Developer!
-    </p>
+    <div>
+      <p className='text-3xl font-bold text-indigo-500'>
+        Hello Developer!
+      </p>
+      <Button>
+        Click Me
+      </Button>
+    </div>
   )
 }
