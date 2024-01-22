21c78a4b99e63082dd9c70e09ab002be654f1fe3
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-22 23:17:26 +0700

    add diagram image

---
+++ prisma/migrations/20240122160002_/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `imageUrl` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Profile_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Server` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `imageUrl` TEXT NOT NULL,
+    `inviteCode` TEXT NOT NULL,
+    `profileId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Server_profileId_idx`(`profileId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Member` (
+    `id` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'MODERATOR', 'GUEST') NOT NULL DEFAULT 'GUEST',
+    `profileId` VARCHAR(191) NOT NULL,
+    `serverId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Member_profileId_idx`(`profileId`),
+    INDEX `Member_serverId_idx`(`serverId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Channel` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `type` ENUM('TEXT', 'AUDIO', 'VIDEO') NOT NULL DEFAULT 'TEXT',
+    `profileId` VARCHAR(191) NOT NULL,
+    `serverId` VARCHAR(191) NOT NULL,
+
+    INDEX `Channel_profileId_idx`(`profileId`),
+    INDEX `Channel_serverId_idx`(`serverId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
