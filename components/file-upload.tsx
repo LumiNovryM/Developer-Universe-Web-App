b33f377e6e283e5d98bc08c4be91b43a8ee12200
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-24 14:58:40 +0700

    feat: create file upload

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -24,6 +24,13 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
                     alt="Upload"
                     className="rounded-full"
                 />
+                <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
             </div>
         )
     }
