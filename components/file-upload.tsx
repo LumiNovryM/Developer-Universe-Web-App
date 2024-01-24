68a041ab66a4a6de52ab25b48662b2e7e43106c0
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-24 14:12:39 +0700

    fix: add uploadthing domain to next.config.js

---
+++ components/file-upload.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  
+    const fileType = value?.split(".").pop();
+    if(value && fileType !== "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image
+                    fill
+                    src={value}
+                    alt="Upload"
+                    className="rounded-full"
+                />
+            </div>
+        )
+    }
+
+    return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -10,7 +11,7 @@ import {
   DialogFooter, 
   DialogHeader, 
   DialogTitle
-} from "../ui/dialog";
+} from "@/components/ui/dialog";
 import { 
   Form,
   FormControl,
@@ -18,10 +19,10 @@ import {
   FormItem,
   FormLabel,
   FormMessage
-} from "../ui/form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import { useEffect, useState } from "react";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -72,7 +73,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField 
+                  control={form.control}
+                  name="imageUrl"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField 
                 control={form.control}
