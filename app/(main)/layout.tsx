015f3b9339139b2264ea2456ccf89ee768c00e7c
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-24 23:58:38 +0700

    wip: add padding left to server page

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+const MainLayout = async ({children}: {children: React.ReactNode}) => {
+    return ( 
+        <div className="h-full">
+            <div className="hidden nd:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+
+            </div>
+            <main className="md:pl-[72px] h-full">
+                {children}
+            </main>
+        </div>
+    );
+}
+ 
+export default MainLayout;
