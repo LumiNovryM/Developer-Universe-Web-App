894f53af25df79f2b0ca99475c06fd9aa4b6f00e
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-26 09:49:22 +0700

    chore: update package & fix audit package

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,7 @@ const MainLayout = async ({children}: {children: React.ReactNode}) => {
     return ( 
         <div className="h-full">
             <div className="hidden nd:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-
+                <NavigationSidebar />
             </div>
             <main className="md:pl-[72px] h-full">
                 {children}
