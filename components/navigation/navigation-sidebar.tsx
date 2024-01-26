503b6a90683f6d9335eea91d3400780317f168a4
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-26 11:32:06 +0700

    feat: create server list, add server tooltip, user profile, & mode-toggle

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,32 +1,56 @@
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
-
+import { UserButton } from "@clerk/nextjs";
 import { currentProfile } from "@/lib/current-profile";
+
 
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
-
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "@/components/navigation/navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const NavigationSidebar = async () => {
+  const profile = await currentProfile();
 
-    const profile = await currentProfile();
+  if (!profile) return redirect("/");
 
-    if(!profile) return redirect("/")
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
-    const servers = await db.server.findMany({
-        where: {
-            members: {
-                some: {
-                    profileId: profile.id
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full w-full text-primary dark:bg-[#1b1b2c] py-3">
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+            afterSignOutUrl="/"
+            appearance={{ 
+                elements: {
+                    avatarBox: "h-[48px] w-[48px]"
                 }
-            }
-        }
-    });
-
-    return (
-        <div className="space-y-4 flex flex-col items-center h-full w-full text-primary dark:bg-[#1b1b2c] py-3">
-            <NavigationAction />
-            <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
-        </div>
-    )
-}
+             }}
+        />
+      </div>
+    </div>
+  );
+};
