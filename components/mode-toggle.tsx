3a37e6685354c97cb4fd2342b5f251f166da792b
LumiNovryM <luminovrymekel@gmail.com>
2024-01-28 11:07:58 +0700

    fix: change mode-toggle style

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -17,14 +17,14 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <DropdownMenuTrigger asChild> 
+        <Button variant="outline" size="icon" className="dark:bg-[#12121e] rounded-[24px] h-[48px] w-[48px] group-hover:bg-blue-500">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-teal-500" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-teal-500" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="dark:bg-[#12121e]">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -12,7 +12,7 @@ export const NavigationAction = () => {
     >
       <div>
         <button className="group flex items-center">
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-[#12121e] group-hover:bg-teal-500">
+          <div className="flex mx-3 h-[48px] w-[48px] border rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-[#12121e] group-hover:bg-teal-500">
             <Plus
               className="group-hover:text-white transition text-teal-500"
               size={25}
