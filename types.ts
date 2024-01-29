2ba338ebb143c4ed23db6c43b12d3613ce8ed322
LumiNovryM <luminovrymekel@gmail.com>
2024-01-29 16:26:36 +0700

    fix: error build

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { Server, Member, Profile } from "@prisma/client"
 
-export type ServerWithMembersWithProfile = Server & {
+export type ServerWithMembersWithProfiles = Server & {
     members: (Member & {profile: Profile})[];
 }
