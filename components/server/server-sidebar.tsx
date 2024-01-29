ea356f55f48672524f2fd0c04c3aa9b3cc8baabf
LumiNovryM <luminovrymekel@gmail.com>
2024-01-29 10:31:44 +0700

    fix: change prisma schema

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,43 +1,43 @@
-// import { currentProfile } from "@/lib/current-profile";
-// import { db } from "@/lib/db";
-// import { redirect } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-// interface ServerSidebarProps {
-//   serverId: string;
-// }
+interface ServerSidebarProps {
+  serverId: string;
+}
 
-// export const ServerSidebar = async ({
-//   serverId
-// }: ServerSidebarProps) => {
+export const ServerSidebar = async ({
+  serverId
+}: ServerSidebarProps) => {
 
-//     const profile = currentProfile();
+    const profile = currentProfile();
 
-//     if (!profile) return redirect("/");
+    if (!profile) return redirect("/");
 
-//     const server = await db.server.findUnique({
-//       where: {
-//         id: serverId,
-//       },
-//       include: {
-//         channels: {
-//           orderBy: {
-//             createdAt: "asc",
-//           },
-//         },
-//         members: {
-//           include: {
-//             profile: true,
-//           },
-//           orderBy: {
-//             role: "asc",
-//           }
-//         }
-//       }
-//     });
+    const server = await db.server.findUnique({
+      where: {
+        id: serverId,
+      },
+      include: {
+        channels: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          }
+        }
+      }
+    });
 
-//     return (
-//         <div>
-//             Server Sidebar Component
-//         </div>
-//     )
-// };
+    return (
+        <div>
+            Server Sidebar Component
+        </div>
+    )
+};
