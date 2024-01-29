f16d6e92011982e1c4ce324a82d3fd734665113b
LumiNovryM <luminovrymekel@gmail.com>
2024-01-29 10:21:10 +0700

    fix: object literal may only specify known properties

---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,43 @@
+// import { currentProfile } from "@/lib/current-profile";
+// import { db } from "@/lib/db";
+// import { redirect } from "next/navigation";
+
+// interface ServerSidebarProps {
+//   serverId: string;
+// }
+
+// export const ServerSidebar = async ({
+//   serverId
+// }: ServerSidebarProps) => {
+
+//     const profile = currentProfile();
+
+//     if (!profile) return redirect("/");
+
+//     const server = await db.server.findUnique({
+//       where: {
+//         id: serverId,
+//       },
+//       include: {
+//         channels: {
+//           orderBy: {
+//             createdAt: "asc",
+//           },
+//         },
+//         members: {
+//           include: {
+//             profile: true,
+//           },
+//           orderBy: {
+//             role: "asc",
+//           }
+//         }
+//       }
+//     });
+
+//     return (
+//         <div>
+//             Server Sidebar Component
+//         </div>
+//     )
+// };
